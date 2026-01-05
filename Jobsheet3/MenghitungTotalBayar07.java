14b20e4b4efbf0d3eaead8fe18da585c0d0a6089
OLIAA <auliazahwaa007@gmail.com>
2026-01-05 20:00:54 +0700

    Percobaan 3

---
+++ Jobsheet3/MenghitungTotalBayar07.java
@@ -0,0 +1,20 @@
+package Jobsheet3;
+
+import java.util.Scanner;
+
+public class MenghitungTotalBayar07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int harga;
+        double potongan, jmlBayar, diskon = 0.15;
+
+        System.out.print("Masukkan harga: ");
+        harga = sc.nextInt();
+
+        potongan = diskon * harga;
+        jmlBayar = harga - potongan;
+
+        System.out.println("Jumlah yang harus anda bayar adalah Rp. " + jmlBayar);
+    }
+}
