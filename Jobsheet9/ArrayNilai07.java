fc4533e20c8340a49a542c0f6ea8cc3a39922ec8
OLIAA <auliazahwaa007@gmail.com>
2026-01-10 11:18:04 +0700

    Percobaan 2

--- Jobsheet9/ArrayNilai07.java
+++ Jobsheet9/ArrayNilai07.java
@@ -8,12 +8,16 @@ public class ArrayNilai07 {
 
         int[] nilaiAkhir = new int[11];
 
-        for(int i = 1; i <= 10; i++) {
+        for(int i = 1; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" + i + ": ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for(int i = 1; i <= 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for(int i = 1; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!!");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " Tidak lulus!");
+            }
         }
     }
 }
