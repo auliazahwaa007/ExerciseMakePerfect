fbf25569cf1b8fb62260247d67bf5e7a9eae921a
OLIAA <auliazahwaa007@gmail.com>
2026-01-29 17:24:31 +0700

    pindah folder lagi

--- AnotherExercise/Pemilihan/ongkir.java
+++
@@ -1,9 +0,0 @@
-package AnotherExercise.Pemilihan;
-
-import java.util.Scanner;
-
-public class ongkir {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-    }
-}
