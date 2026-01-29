a5076055e0cde8447a39feb9759f44be30195ba6
OLIAA <auliazahwaa007@gmail.com>
2026-01-29 16:59:53 +0700

    coba

---
+++ AnotherExercise/Pemilihan/ongkir.java
@@ -0,0 +1,9 @@
+package AnotherExercise.Pemilihan;
+
+import java.util.Scanner;
+
+public class ongkir {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
