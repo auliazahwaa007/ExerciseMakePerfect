5b9bbb176dcf23dd5c90c5968e94725c80a8f7e1
OLIAA <auliazahwaa007@gmail.com>
2026-01-29 16:55:51 +0700

    ulang

---
+++ AnotherExercise/ecommrce.java
@@ -0,0 +1,22 @@
+package AnotherExercise;
+
+import java.util.Scanner;
+
+public class ecommrce {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Apakah pembayaran sudah lunas? (True/False): ");
+        boolean statusPembayaran = sc.nextBoolean();
+        System.out.print("Apakah stok tersedia? (True/False): ");
+        boolean stok = sc.nextBoolean();
+
+        if (statusPembayaran == true && stok == true) {
+            System.out.println("Pesanan Anda sedang diproses.");
+        } else if (statusPembayaran == false) {
+            System.out.println("Sedang menunggu pembayaran.");
+        } else if (stok == false) {
+            System.out.println("Maaf stok habis.");
+        }
+    }
+}
