5a514255a6abdc2e0958f0514ca1c6f73107cbb2
OLIAA <auliazahwaa007@gmail.com>
2026-01-29 11:23:00 +0700

    if-else

---
+++ AnotherExercise/checkKelulusan.java
@@ -0,0 +1,20 @@
+package AnotherExercise;
+
+import java.util.Scanner;
+
+public class checkKelulusan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== PENGECEKAN KELULUSAN =====");
+        System.out.print("Masukkan nilai Anda: ");
+        int nilai = sc.nextInt();
+
+        if (nilai >= 75) {
+            System.out.println("Selamat Anda Lulus!!!");
+        } else {
+            System.out.println("Nilai Anda belum mencukupi silahkan mencoba kembali.");
+        }
+        sc.close();
+    }
+}
